1ea095db07e5058b372c2d58f75fc7aaa0d8d6c4
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2023-01-04 17:35:15 -0300

    Add: rota - exibição e deleção de contatos

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -8,8 +8,17 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
     // Exibir UM registro
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found.' });
+    }
+
+    response.json(contact);
   }
 
   store() {
@@ -20,8 +29,18 @@ class ContactController {
     // Editar um registro
   }
 
-  delete() {
+  async delete(request, response) {
     // Deletar um registro
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found.' });
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Mateus',
     email: 'mateus@mail.com',
     phone: '112233445',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Beatriz',
+    email: 'beatriz@mail.com',
+    phone: '912233445',
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +21,21 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.id === id),
+      );
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
