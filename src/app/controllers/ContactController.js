79a19bbf3f96e8c937ddba8ffe7480c8603f68ee
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2023-01-04 21:57:50 -0300

    Add: rota - edição de contato

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -34,7 +34,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already been taken.' });
+      return response.status(400).json({ error: 'This e-mail is already in use.' });
     }
 
     const contact = await ContactsRepository.create({
@@ -44,8 +44,32 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+    if (!contactExists) {
+      response.status(404).json({ error: 'User not found.' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required.' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use.' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
